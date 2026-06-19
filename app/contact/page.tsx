b2901a31f7ee63b1be91503034d3b417b01ff8fd
Pushkar0730 <pushkar.bhegade18@gmail.com>
2026-06-19 23:44:11 +0530

    Database changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -61,7 +61,7 @@ export default function Contact() {
       }
     } catch (error) {
       console.error(error);
-      alert("Something went wrong.");
+      alert(JSON.stringify(error));
     }
   };
 
