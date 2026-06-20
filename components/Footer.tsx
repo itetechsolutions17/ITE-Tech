9a61b78819771f265eb05685fe2446780288d68e
Pushkar0730 <pushkar.bhegade18@gmail.com>
2026-06-21 00:57:41 +0530

    Footer minor Changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -109,7 +109,7 @@ export default function Footer() {
       </p>
 
       <p className="text-sm text-slate-500">
-        Designed & Developed by ITE Tech Solutions 🚀
+        Designed & Developed by ITE Tech Solutions |
       </p>
     </div>
   </footer >
