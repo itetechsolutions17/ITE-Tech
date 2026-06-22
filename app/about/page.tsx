c60192ff6b4e414ad01a08aab2f44fd2d7a4402e
Pushkar0730 <pushkar.bhegade18@gmail.com>
2026-06-22 20:37:03 +0530

    Director Profile Add

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -270,7 +270,7 @@ export default function About() {
 
                   <p className="text-2xl md:text-3xl font-light leading-relaxed text-gray-200 italic">
 
-                    <p className="absolute text-4xl text-blue-500/20 font-serif">
+                    <p className="text-4xl text-blue-500/20 font-serif">
                       "
                     </p>
 
@@ -278,7 +278,7 @@ export default function About() {
                     it's about creating solutions that empower people,
                     accelerate growth, and transform ideas into reality.
 
-                    <p className="absolute text-4xl text-blue-500/20 font-serif">
+                    <p className="text-4xl text-blue-500/20 font-serif">
                       "
                     </p>
 
